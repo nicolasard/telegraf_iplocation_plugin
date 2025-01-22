5c3985da23cfd0e1bf06abb28f7e61580d7ccd93
Nic.Ard <nicolas.ard@gmail.com>
2025-01-22 20:43:55 +0100

    fixing

--- src/main/java/ar/nic/IpLocationService.java
+++ src/main/java/ar/nic/IpLocationService.java
@@ -9,18 +9,18 @@ public class IpLocationService {
 
     private final String binaryFileLocation;
 
-    private final boolean loadInMemmory;
+    private final boolean loadInMemory;
 
     private final IP2Location loc = new IP2Location();
 
     public IpLocationService() {
-        this.binaryFileLocation = "/opt/IP2LOCATION-LITE-DB9.IPV6.BIN";
-        this.loadInMemmory = false;
+        this.binaryFileLocation = System.getenv().getOrDefault("IP2LOCATION_BINARY_FILE", "/opt/IP2LOCATION-LITE-DB9.IPV6.BIN");        this.loadInMemory = false;
+        this.loadInMemory = Boolean.parseBoolean(System.getenv().getOrDefault("IP2LOCATION_LOAD_IN_MEMORY", "false"));this.loadInMemory = Boolean.parseBoolean(System.getenv().getOrDefault("IP2LOCATION_LOAD_IN_MEMORY", "false"));private final boolean loadInMemory;
     }
 
     public IPResult getIpInformation(final String ip){
         try {
-            loc.Open(this.binaryFileLocation, loadInMemmory);
+            loc.Open(this.binaryFileLocation, loadInMemory);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
